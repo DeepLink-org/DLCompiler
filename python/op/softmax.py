b76b7eec113b020eacf8057e39ddb11bc1b60b6b
yuansheng <yuansheng@pjlab.org.cn>
2024-06-17 11:57:17 +0800

    fix compile issue

--- python/op/softmax.py
+++ python/op/softmax.py
@@ -2,6 +2,19 @@ import torch
 
 import triton
 import triton.language as tl
+import triton.compiler as tc
+
+# from triton.backends.triton.driver import DICPDriver
+# from triton.common.backend import register_backend
+import triton.backends.dicp.driver as dicp
+import triton.compiler as tc
+from pathlib import Path
+# import backend.driver as bd
+
+
+triton.runtime.driver.set_active(dicp.DICPDriver())
+# triton.runtime.driver.set_active(cu.CudaDriver())
+# register_backend("dicp", cl.DICPBackend("mlu"))
 
 @triton.jit
 def softmax_native_kernel(output_ptr, input_ptr, input_row_stride, 
@@ -34,7 +47,7 @@ def get_configs_io_bound():
     
     return configs
 
-@triton.autotune(configs=get_configs_io_bound(), key=['n_cols'])
+# @triton.autotune(configs=get_configs_io_bound(), key=['n_cols'])
 @triton.jit
 def softmax_optimize_kernel(output_ptr, input_ptr, input_row_stride, 
                            output_row_stride, n_rows, n_cols, 
@@ -62,6 +75,84 @@ def softmax_optimize_kernel(output_ptr, input_ptr, input_row_stride,
 
         tl.store(output_ptrs, softmax_output, mask = col_offsets < n_cols)
 
+# @triton.autotune(
+#     configs=[
+#         triton.Config(kwargs={'BLOCK_SIZE': 4096}, num_stages=1, num_warps=8),
+#         triton.Config(kwargs={'BLOCK_SIZE': 10240}, num_stages=1, num_warps=4),
+#         triton.Config(kwargs={'BLOCK_SIZE': 18432}, num_stages=1, num_warps=4),
+#         triton.Config(kwargs={'BLOCK_SIZE': 32768}, num_stages=1, num_warps=4),
+#         triton.Config(kwargs={'BLOCK_SIZE': 43520}, num_stages=1, num_warps=4),
+#     ],
+#     key=['n_elements'],
+# )
+@triton.jit
+def add_kernel(
+    x_ptr,
+    y_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    """
+        x_ptr: *Pointer* to first input vector.
+        y_ptr: *Pointer* to second input vector.
+        output_ptr: *Pointer* to output vector.
+        n_elements: Size of the vector.
+        BLOCK_SIZE: Number of elements each program should process. `constexpr` so it can be used as a shape value.
+    """
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    # Write x + y back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    # We need to preallocate the output.
+    output = torch.empty_like(x)
+    # assert x.is_mlu and y.is_mlu and output.is_mlu
+    n_elements = output.numel()
+    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+    # It is analogous to MLU launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+    # In this case, we use a 1D grid where the size is the number of blocks:
+    # grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable MLU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    add_kernel[(16,)](x, y, output, n_elements, BLOCK_SIZE=1024)
+    # We return a handle to z but, since `torch.mlu.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+
+# torch.manual_seed(0)
+# size = 98432
+# x = torch.rand(size, device='cuda')
+# y = torch.rand(size, device='cuda')
+# # output_torch = x + y
+# output_triton = add(x, y)
+# src = tc.ASTSource(
+#     fn=add_kernel,
+#     constants={"BLOCK_SIZE": 1024},
+#     signature="*fp32, *fp32, *fp32, i32",
+# )
+# ret = triton.compile(src)
+# src_path = "add_kernel.mlir"
+# str = ret.asm["ttlinalgdir"]
+# Path(src_path).write_bytes(ret.asm["ttlinalgdir"])
+
 
 def softmax(x):
     n_rows, n_cols = x.shape
@@ -74,7 +165,12 @@ def softmax(x):
                                     OUTER_ROW_BLOCK=OUTER_ROW_BLOCK)
     return y
 
-torch.manual_seed(0)
-x = torch.randn(1823, 781, device='cuda')
-y_triton = softmax(x)
-y_torch = torch.softmax(x, axis=1)
+src = tc.ASTSource(
+    fn=softmax_optimize_kernel,
+    constants={"BLOCK_SIZE": 64, "OUTER_ROW_BLOCK": 8, "ROW_BLOCK": 1},
+    signature="*fp32, *fp32, i32, i32, i32, i32",
+)
+ret = triton.compile(src)
+src_path = "softmax_optimize_kernel.mlir"
+str = ret.asm["ttlinalgdir"]
+Path(src_path).write_bytes(ret.asm["ttlinalgdir"])

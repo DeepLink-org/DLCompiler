7ba346b33f0651cde80458a989a4e4d8cd31fb06
Your Name <you@example.com>
2024-07-01 11:17:19 +0000

    refactor

--- python/dlBLAS/dlblas/frontend.py
+++ python/dlBLAS/dlblas/frontend.py
@@ -1,16 +1,1 @@
-from dlblas.op_registry import op_registry
 
-
-def get_list_op_names() -> list[str]:
-    return op_registry.get_list_op_names()
-
-
-def get_args_from_op_name(name: str):
-    return op_registry.get_args_from_op_name(name)
-
-
-def get_op(name: str, args):
-    '''based on name and args,
-    return OpImpl
-    '''
-    return op_registry.get_op(name, args)

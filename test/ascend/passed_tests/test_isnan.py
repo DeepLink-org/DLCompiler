fix: handle empty call, multi-return, i1 bitcast, and i1/i8 addition overflow issues

test/ascend/passed_tests/test_isnan.py
@@ -35,6 +35,7 @@ shapes = [
     256,
 ]
 
+
 @pytest.mark.parametrize("sigtype", types)
 @pytest.mark.parametrize("N", shapes)
 def test_isnan(sigtype, N):
@@ -43,7 +44,6 @@ def test_isnan(sigtype, N):
         res = torch.isnan(x0)
         return res
 
-
     @triton.jit
     def triton_kernel(out_ptr0, in_ptr0, N: tl.constexpr):
         idx = tl.arange(0, N)
@@ -51,19 +51,19 @@ def test_isnan(sigtype, N):
         ret = tl.extra.deeplink.libdevice.isnan(x0)
         tl.store(out_ptr0 + idx, ret)
 
-
     def triton_func(x0, N):
         out = torch.zeros(x0.shape, dtype=torch.bool).npu()
         triton_kernel[1, 1, 1](out, x0, N)
         return out
 
     x0 = test_common.generate_tensor(shape=(N,), dtype=sigtype).npu()
-    x0[1] = float('nan')
+    x0[1] = float("nan")
 
     torch_ref = torch_func(x0)
     triton_cal = triton_func(x0, N)
     test_common.validate_cmp("bool", triton_cal, torch_ref)
     assert triton_cal[1] == True
+
 
 # invalid_types = [
 #     "int32",
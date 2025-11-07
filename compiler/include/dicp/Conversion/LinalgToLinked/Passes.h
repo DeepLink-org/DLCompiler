704aa8b8a7ad59aaed06dae32545b4a1b262ca09
TruthK <kezengxiang@pjlab.org.cn>
2025-11-07 02:28:30 +0000

    chore: remote GEN_PASS_DECL

--- compiler/include/dicp/Conversion/LinalgToLinked/Passes.h
+++ compiler/include/dicp/Conversion/LinalgToLinked/Passes.h
@@ -3,8 +3,6 @@
 #include "dicp/Conversion/LinalgToLinked/LinalgToLinked.h"
 
 namespace mlir::dicp::linked {
-#define GEN_PASS_DECL
-#include "dicp/Conversion/LinalgToLinked/Passes.h.inc"
 
 #define GEN_PASS_REGISTRATION
 #include "dicp/Conversion/LinalgToLinked/Passes.h.inc"

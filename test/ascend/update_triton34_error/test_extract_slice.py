4afec348e4b9e632b0448a19433c747f80064c0a
hellozmz <407190054@qq.com>
2025-11-18 10:17:17 +0000

    ci

--- test/ascend/update_triton34_error/test_extract_slice.py
+++ test/ascend/update_triton34_error/test_extract_slice.py
@@ -4,6 +4,7 @@ import triton
 import triton.language as tl
 import triton.language.extra.deeplink as dl
 
+
 @triton.jit
 def triton_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
     pid = tl.program_id(axis=0)
@@ -18,18 +19,20 @@ def triton_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr
     out_msk = out_idx < n_elements
     tl.store(output_ptr + out_idx, out_sub, mask=out_msk)
 
+
 def triton_func(x: torch.Tensor, y: torch.Tensor):
     output = torch.empty_like(x)
     n_elements = output.numel()
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
     triton_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
     return output
 
+
 def test_extract_slice():
     size = 1024
-    x = torch.rand(size, device='npu')
-    y = torch.rand(size, device='npu')
+    x = torch.rand(size, device="npu")
+    y = torch.rand(size, device="npu")
     torch_ref = x + y
     triton_cal = triton_func(x, y)
-    print('max diff', (triton_cal[:32] - torch_ref[:32]).abs().max())
+    print("max diff", (triton_cal[:32] - torch_ref[:32]).abs().max())
     torch.testing.assert_close(triton_cal[:32], torch_ref[:32])

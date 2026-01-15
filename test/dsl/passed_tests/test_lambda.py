d3410834726cfaf15f2da0d28360c98938e9f660
hellozmz <407190054@qq.com>
2026-01-15 06:41:29 +0000

    add dsl pytest

--- test/dsl/passed_tests/test_lambda.py
+++ test/dsl/passed_tests/test_lambda.py
@@ -3,14 +3,15 @@ import triton
 import triton.language as tl
 import pytest
 
+
 @triton.jit
-def add_kernel(x_ptr,  # *Pointer* to first input vector.
-               y_ptr,  # *Pointer* to second input vector.
-               output_ptr,  # *Pointer* to output vector.
-               n_elements,  # Size of the vector.
-               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-               # NOTE: `constexpr` so it can be used as a shape value.
-               ):
+def add_kernel(
+    x_ptr,  # *Pointer* to first input vector.
+    y_ptr,  # *Pointer* to second input vector.
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+):
     # There are multiple 'programs' processing different data. We identify which program
     # we are here:
     pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
@@ -33,31 +34,37 @@ def add_kernel(x_ptr,  # *Pointer* to first input vector.
     # Write x + y back to DRAM.
     tl.store(output_ptr + offsets, output, mask=mask)
 
+
 def add(x: torch.Tensor, y: torch.Tensor):
     output = torch.empty_like(x)
     n_elements = output.numel()
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
     add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=128)
     return output
 
-def test_add():
+
+@pytest.mark.parametrize("size", [1024])
+def test_add(size):
     torch.manual_seed(0)
-    size = 1024
-    x = torch.rand(size, device='npu')
-    y = torch.rand(size, device='npu')
+    x = torch.rand(size, device="npu")
+    y = torch.rand(size, device="npu")
     output_torch = x + y
     output_triton = add(x, y)
     # print(output_torch)
     # print(output_triton)
     assert torch.allclose(output_torch, output_triton, atol=1e-5)
-    print(f'The maximum difference between torch and triton is '
-        f'{torch.max(torch.abs(output_torch - output_triton))}')
+    print(
+        f"The maximum difference between torch and triton is "
+        f"{torch.max(torch.abs(output_torch - output_triton))}"
+    )
+
 
 NBLOCKS = 1
-XS : tl.constexpr = 128
-YS : tl.constexpr = 4
-ZS : tl.constexpr = 8
-NUMEL : tl.constexpr = XS * ZS
+XS = tl.constexpr(128)
+YS = tl.constexpr(4)
+ZS = tl.constexpr(8)
+NUMEL = tl.constexpr(XS * ZS)
+
 
 @triton.jit
 def fn_broadcast(output_ptr, x_ptr, length):
@@ -65,29 +72,39 @@ def fn_broadcast(output_ptr, x_ptr, length):
     input = tl.load(x_ptr + col_offsets)
     count = lambda a, b, c: a * b * c
     # result = input.reshape((XS, 1, ZS)).broadcast_to((XS, YS, ZS)).reshape((XS * YS * ZS))
-    result = input.reshape((XS, 1, ZS)).broadcast_to((XS, YS, ZS)).reshape((count(XS, YS, ZS)))
+    result = (
+        input.reshape((XS, 1, ZS))
+        .broadcast_to((XS, YS, ZS))
+        .reshape((count(XS, YS, ZS)))
+    )
     brc_col_offsets = tl.arange(0, NUMEL * YS)
     tl.store(output_ptr + brc_col_offsets, result)
 
-def test_broadcast():
+
+@pytest.mark.parametrize("nblocks", [NBLOCKS])
+def test_broadcast(nblocks):
     length = NUMEL
 
     x = torch.randn((XS, 1, ZS), dtype=torch.float32).npu()
     output = torch.randn((XS, YS, ZS), dtype=torch.float32).npu()
-    fn_broadcast[NBLOCKS,1,1](output, x, length, debug=True)
-    assert(torch.equal(output, x.repeat(1, YS, 1)))
+    fn_broadcast[nblocks, 1, 1](output, x, length, debug=True)
+    assert torch.equal(output, x.repeat(1, YS, 1))
+
 
 @triton.jit
-def custom_copy_kernel(input_ptr, output_ptr,
-                        input_row_size: tl.constexpr,  # 输入矩阵行维度大小 (128)
-                        block_size: tl.constexpr):     # 块大小 (16)
+def custom_copy_kernel(
+    input_ptr,
+    output_ptr,
+    input_row_size: tl.constexpr,  # 输入矩阵行维度大小 (128)
+    block_size: tl.constexpr,
+):  # 块大小 (16)
     base_idx = tl.arange(0, block_size)
 
     # 通过lambda计算带步长的输入索引
     stride_idx = lambda x: x * 8  # 每隔8个元素
 
     input_rows = stride_idx(base_idx)[:, None]  # [16,1]
-    input_cols = stride_idx(base_idx)[None, :]   # [1,16]
+    input_cols = stride_idx(base_idx)[None, :]  # [1,16]
     # input_rows = (base_idx * 8)[:, None]
     # input_cols = (base_idx * 8)[None, :]
 
@@ -97,23 +114,24 @@ def custom_copy_kernel(input_ptr, output_ptr,
     data = tl.load(input_ptr + input_pos)
     tl.store(output_ptr + output_pos, data)
 
-def test_custom_copy():
+
+@pytest.mark.parametrize("input_shape,block_size", [((128, 128), 16)])
+def test_custom_copy(input_shape, block_size):
     # 创建输入张量 (128x128) 和输出张量 (16x16)
-    input_tensor = torch.randn(128, 128, device='npu')
-    output_tensor = torch.zeros(16, 16, device='npu')
+    input_tensor = torch.randn(*input_shape, device="npu")
+    output_tensor = torch.zeros(block_size, block_size, device="npu")
 
     grid = (1, 1)
-    custom_copy_kernel[grid](input_tensor, output_tensor,
-                              input_row_size=128,
-                              block_size=16)
+    custom_copy_kernel[grid](
+        input_tensor,
+        output_tensor,
+        input_row_size=input_shape[1],
+        block_size=block_size,
+    )
 
-    expected = input_tensor[::8, ::8][:16, :16]
-    print(f"Expected shape: {expected}, Output shape: {output_tensor}")
-    assert torch.allclose(output_tensor, expected, atol=1e-6), \
-        "Copied data does not match the custom 16x16 block"
+    expected = input_tensor[::8, ::8][:block_size, :block_size]
+    # print(f"Expected shape: {expected}, Output shape: {output_tensor}")
+    assert torch.allclose(
+        output_tensor, expected, atol=1e-6
+    ), "Copied data does not match the custom 16x16 block"
     print("Test passed: 16x16 block copied correctly.")
-
-if __name__ == "__main__":
-    test_add()
-    test_broadcast()
-    test_custom_copy()
---
+++ test/dsl/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+script=$(readlink -f "$0")
+script_dir=$(dirname "$script")
+
+function run_pytestcases() {
+  if [ -d ${HOME}/.triton/dump ]; then
+    rm -rf ${HOME}/.triton/dump
+  fi
+  if [ -d ${HOME}/.triton/cache ]; then
+    rm -rf ${HOME}/.triton/cache
+  fi
+
+  cd ${script_dir}
+  TARGET_DIR="$1"
+  cd ${TARGET_DIR}
+  pytest -n 16 --dist=load . || { exit 1 ; }
+
+}
+
+pytestcase_dir=("passed_tests")
+for test_dir in "${pytestcase_dir[@]}"; do
+    echo "run pytestcase in ${test_dir}"
+    run_pytestcases ${test_dir}
+done
+
+
+
